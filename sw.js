3e89165a1da2b75f0f69cdc8ec83aa0ca571e1c9
ekosistema <46524536+ekosistema@users.noreply.github.com>
2023-04-28 20:49:28 +0200

    PWA and OpenGraph
    PWA and OpenGraph

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "my-cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) =>
+        cache.addAll(["/", "/index.html", "/styles.css", "/app.js"])
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
